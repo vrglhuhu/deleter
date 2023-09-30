77456df1ec803c7382b81320140bdfd762d7d829
vrglhuhu <cheanbvergel@gmail.com>
2023-09-30 23:16:22 +0800

    Psudocodes.

---
+++ duplicate_remover.py
@@ -0,0 +1,14 @@
+# VERGEL, CHEAN BERNARD V.
+# LAB1 NO2
+# DUPLICATE REMOVER
+
+# Create class called remove_duplicates
+# Function to remove duplicate characters from a string
+        # Create an empty set to keep track of unique characters
+        # Create an empty string to store the result
+        # Iterate through each character in the input string
+            # Check if the character is not in the unique_chars set
+                # Add the character to the unique_chars set
+                # Append the character to the result string
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+# VERGEL, CHEAN BERNARD V.
+from duplicate_remover import remove_duplicates
+
+
+# Get the number of strings as input
+# Initialize a list to store the input strings
+# Loop to get N strings from the user
+# Iterate through the list of input strings and remove duplicates
