7a2609a802792076b934288767842348430e5e98
vrglhuhu <cheanbvergel@gmail.com>
2023-09-30 23:21:35 +0800

    adding the code.

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -3,12 +3,23 @@
 # DUPLICATE REMOVER
 
 # Create class called remove_duplicates
+class remove_duplicates:
 # Function to remove duplicate characters from a string
+    def remove_char(input_str):
         # Create an empty set to keep track of unique characters
+        unique_chars = set()
+        
         # Create an empty string to store the result
+        result_str = ""
+        
         # Iterate through each character in the input string
+        for char in input_str:
             # Check if the character is not in the unique_chars set
+            if char not in unique_chars:
                 # Add the character to the unique_chars set
+                unique_chars.add(char)
                 # Append the character to the result string
-
+                result_str += char
+        
+        return result_str
 
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 # VERGEL, CHEAN BERNARD V.
+# VERGEL, CHEAN BERNARD V.
+# LAB1 NO2
+# DUPLICATE REMOVER
+
+# Import the remove_duplicates
 from duplicate_remover import remove_duplicates
 
-
 # Get the number of strings as input
+n = int(input("Enter the number of strings that you wanted to input: "))
+
 # Initialize a list to store the input strings
+input_strings = []
+
 # Loop to get N strings from the user
+for i in range(n):
+    input_str = input(f"Enter string {i + 1}: ")
+    input_strings.append(input_str)
+
 # Iterate through the list of input strings and remove duplicates
+for input_str in input_strings:
+    result_str = remove_duplicates(input_str)
+    print(f"Result: {result_str}")
