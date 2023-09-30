6ad5cfc35f50a69b0de2420292d8681435b56c36
vrglhuhu <cheanbvergel@gmail.com>
2023-10-01 07:24:03 +0800

    Correcting some parts because it does not delete all the duplicate character.

--- main.py
+++ main.py
@@ -5,18 +5,52 @@
 # Import the remove_duplicates
 from duplicate_remover import RemoveDuplicates
 
+# Create a header.
+import pyfiglet
+print("")
+print("=" * 80)
+print("")
+welcome = pyfiglet.figlet_format("Welcome to my space".center(57, " "), font = "digital" )
+print(welcome)
+print("")
+print("=" * 80)
+print("\033[33mHi, I am Chean Bernard V. Vergel a second year college student at Polytechnic University of the Philippines.\033[0m")
+print("")
+
+# Ask for the name of the user.
+name_of_user = input("\033[35mHow about you what is your name?\U0001F917\033[0m ")
+print("")
+print("=" * 80)
+print("")
+
+# Make a greeting message for the user.
+print("\033[23mGood day,",name_of_user,"this program will ask you to input some words and delete numbers and the letters that are being repeated.\033[0m")
+print("")
+
+
+# Ask if the user wants to continue.
+agreement = str(input("\033[96mDo you want to continue answering this program? Type \033[0m\033[40m\033[33mYES\033[0m\033[96m if you want to continue and type \033[0m\033[40m\033[33mNO\033[0m\033[96m if not.\033[0m "))
+print("")
+print("=" * 80)
+print("")
+
+# If user wants to continue, ask the user to input characters.
+if agreement.upper() == "YES":
+
+
 # Get the number of strings as input
-n = int(input("Enter the number of strings that you wanted to input: "))
+    n = int(input("Enter the number of strings that you want to input: "))
 
 # Initialize a list to store the input strings
-input_strings = []
+    input_strings = []
 
 # Loop to get N strings from the user
 for i in range(n):
     input_str = input(f"Enter string {i + 1}: ")
+    duplicate_removed = RemoveDuplicates.remove_chars(input_str)
     input_strings.append(input_str)
+    input_strings.append(duplicate_removed.replace(" ", ""))
 
 # Iterate through the list of input strings and remove duplicates
 for input_str in input_strings:
-    result_str = RemoveDuplicates.remove_chars(input_str)
-    print(f"Result: {result_str}")
+    print(f"Result: {input_str}")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,6 @@
-# VERGEL, CHEAN BERNARD V.
-# LAB1 NO2
-# DUPLICATE REMOVER
-
-# Create class called RemoveDuplicates
+# Create class called remove_duplicates
 class RemoveDuplicates:
-
-    @staticmethod
-    # Function to remove duplicate characters from a string
+# Function to remove duplicate characters from a string
     def remove_chars(input_str):
         # Create an empty set to keep track of unique characters
         unique_chars = set()
@@ -16,13 +10,11 @@ class RemoveDuplicates:
 
         # Iterate through each character in the input string
         for char in input_str:
-            # Check if the character is not in the unique_chars set, not a digit,
-            # and the count of the character in the input_str is 1
-            if not char.isdigit() and input_str.count(char) == 1 and char not in unique_chars:
+           # Check if the character is not in the unique_chars set and not a digit
+            if char not in unique_chars and not char.isdigit():
                 # Add the character to the unique_chars set
                 unique_chars.add(char)
                 # Append the character to the result string
                 result_str += char
 
-        # use join method to return the result
         return result_str
